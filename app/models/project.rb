0936955d22e92883c01eda394036c814c30e0a0d
AdrianPita <adrianpitafernandez@gmail.com>
2014-03-03 16:54:15 +0100

    Initial Commit

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ActiveRecord::Base
+
+	def self.iron_find(id_projects)
+		#Otra forma para implementar a nivel de clase
+		#find(id_projects)
+		where(id: id_projects).first
+	end
 end

a7651aedad1e0531f5f97f47cbf8d756a734960a
AdrianPita <adrianpitafernandez@gmail.com>
2014-03-03 16:56:08 +0100

    Initial Commit

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
 
 	def self.iron_find(id_projects)
-		#Otra forma para implementar a nivel de clase
+		#Otra forma para implementar a nivel de clase un metodo estatico
 		#find(id_projects)
 		where(id: id_projects).first
 	end
